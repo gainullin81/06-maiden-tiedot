c72fb37df8da7880b445cf2a97c3a18ae1e4d6be
alex <againullin8@gmail.com>
2025-02-03 22:47:31 +0200

    Удалена папка dist локально

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/06-maiden-tiedot",
+  base: "/06-maiden-tiedot/",
   plugins: [react()],
-})
+});
